ce7a3ab9cb97c96a8353c43984abe9ff73a536f0
seoyoungseung <areema@naver.com>
2020-07-20 22:23:06 +0900

    add: jwt token 적용

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path, include
 from accounts.views import ProfileViewSet, UserViewSet
 from rest_framework.routers import DefaultRouter
+from rest_framework_jwt.views import (
+    obtain_jwt_token,
+    refresh_jwt_token,
+    verify_jwt_token,
+)
 
 router = DefaultRouter()
 router.register("profile", ProfileViewSet)
 router.register("user", UserViewSet)
 
 
-urlpatterns = [path("", include(router.urls))]
+urlpatterns = [
+    path("", include(router.urls)),
+    path("api-token-auth/", obtain_jwt_token),
+    path("api-token-refresh/", refresh_jwt_token),
+    path("api-token-verify/", verify_jwt_token),
+]
 
--- settings/common.py
+++ settings/common.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -100,9 +101,18 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
-    ]
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+    ),
+}
+
+JWT_AUTH = {
+    "JWT_ALLOW_REFRESH": True,
+    "JWT_EXPIRATION_DELTA": datetime.timedelta(days=7),
+    "JWT_REFRESH_EXPIRATION_DELTA": datetime.timedelta(days=8),
 }
